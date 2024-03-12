ce232a675ba6e156db58da96319ba8ac1d94c89b
Firoz <firozvashraf@gmail.com>
2024-03-12 22:17:38 +0530

    new add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login  from './components/login.jsx';
 function App() {
   return (
     <div className="App">
+      
       <Login/>
     </div>
   );
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,22 +1,26 @@
 import React from "react"
 import './login.css'
-import './background.jsx'
+import './background.css'
 
 
 
 function Login() {
     return(
-        <div className="mainbox">
+        
         <div className="container">
             <input className="bar1" placeholder="Email"/>
             <input className="bar2" placeholder="Password"/>
-            <div className="box1" >
-             <p className="or">or <a href="">create new account</a></p>
-             
+            <br />
+            <button className="sninbtn">Sign in</button>
+            <p className="text1"><h5>Forgot your password ?</h5></p>
+
+            
+            <div className="box1">
+                <p className="text2"><h5>Don't have an account ?</h5></p>
+                <button className="snupbtn">Sign up</button>
             </div>
-            <button className="logbtn">Login</button>
         </div>
-        </div>
+       
   
     );
 }
